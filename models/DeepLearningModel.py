c24bb8c1afac90663159d5f07cb160e8315c482a
Noah El Rimawi-Fine <noah.elrimawi-fine@mail.mcgill.ca>
2023-12-14 13:24:50 -0500

    adding one-hot-encoding

--- models/DeepLearningModel.py
+++ models/DeepLearningModel.py
@@ -4,10 +4,23 @@ from keras.models import Sequential
 from keras.layers import Conv1D, MaxPooling1D, LSTM, Dense, Flatten
 from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
+import numpy as np
 
 # dataset, pre-processed and loaded here (after Adam's update)
 X = ...  # input features; 3D for CNN (samples, timesteps, features)
 y = ...  # output labels
+
+# one hot encode the the output labels
+def one_hot_encode_seq(sequence, max_length):
+    mapping = {'A': 0, 'C': 1, 'G': 2, 'T': 3, 'N': 4}
+
+    # Pad sequence with 'N' if it's shorter than max_length
+    padded_sequence = sequence + 'N' * (max_length - len(sequence))
+
+    seq_encoded = [mapping[nuc] for nuc in padded_sequence]
+    one_hot_matrix = np.eye(5) 
+    return one_hot_matrix[seq_encoded]
+
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -16,7 +29,7 @@ y_train, y_test = to_categorical(y_train), to_categorical(y_test)  # classificat
 # building the model (CNN + LSTM)
 model = Sequential()
 
-# CNN layer
+# CNN layer -- make sure the input shape matches the shape of the input data
 model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(X_train.shape[1], X_train.shape[2])))
 model.add(MaxPooling1D(pool_size=2))
 
